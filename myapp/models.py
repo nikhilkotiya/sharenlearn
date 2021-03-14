a2ceae7b161b2b56dda5cc7a743d5170d984e003
nikhilkotiya <nikhilkotiya8@gmail.com>
2021-03-14 17:04:21 +0530

    first commit

--- myapp/models.py
+++ myapp/models.py
@@ -13,12 +13,5 @@ class Signup(models.Model):
 class Notes(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     uploadingdate = models.CharField(max_length=30)
-    branch = models.CharField(max_length=50)
-    subject = models.CharField(max_length=50)
-    notesfile = models.FileField(null=True)
-    filetype = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
-    status = models.CharField(max_length=15)
-
     def __str__(self):
         return self.user.username+" "+self.status
